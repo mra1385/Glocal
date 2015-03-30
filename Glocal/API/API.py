5e798ff831802387daaafc3ecc6276e4d7ae5cf2
mra1385 <mikeazar@gmail.com>
2015-03-29 21:29:58 -0400

    made fix to exception handling for eventbrite (if no events)

--- Glocal/API/API.py
+++ Glocal/API/API.py
@@ -197,16 +197,16 @@ class GlocalAPI:
                                                        'popular':'true'})
 
         lst_events = []
-        try:
-            for i in xrange(20):
+        for i in xrange(20):
+            try:
                 tmp_event = []
                 tmp_event.append(eventbrite_events['events'][i]['name']['text'])
                 tmp_event.append(eventbrite_events['events'][i]['venue']['name'])
                 tmp_event.append(eventbrite_events['events'][i]['start']['local'])
                 tmp_event.append(eventbrite_events['events'][i]['url'])
                 lst_events.append(tmp_event)
-        except:
-            continue
+            except:
+                continue
 
         if eventful_events['events'] != None:
             if isinstance(eventful_events['events']['event'], list):

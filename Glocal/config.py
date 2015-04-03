332d9305f303fd95768ca817ad88948879cbdc4f
mra1385 <mikeazar@gmail.com>
2015-04-03 12:12:53 -0400

    misc fixes

--- Glocal/config.py
+++ Glocal/config.py
@@ -2,5 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 APP_NAME = 'Glocal'
-CHOSEN_MEDIA = ['Twitter', 'Instagram', 'Four Square']
+CHOSEN_MEDIA = ['Twitter', 'Instagram', 'Four Square',
+                'LastFM', 'Eventful', 'Eventbrite']
 
